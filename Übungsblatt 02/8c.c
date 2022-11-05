18c75e52f829f0ab6becdac3a26265635654d361
fabiiiooooooooo <114263160+fabiiiooooooooo@users.noreply.github.com>
2022-11-05 15:11:06 +0100

    Aufgaben Blatt aktualisiert

--- Übungsblatt 02/8c.c
+++ Übungsblatt 02/8c.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int sum2n(int a);
+int sum2a(int a);
 
 int main(void)
 {
 	int a;
 	printf("Eingabe von a\n");
 	scanf("%i", &a);
-	printf("a: %i\tf(a): %i\n", a, sum2n(a));
+	printf("a: %i\tf(a): %i\n", a, sum2a(a));
 
 
 	return 0;
 }
 
-int sum2n(int a)
+int sum2a(int a)
 {
 	int b = 0;
 	int c = 0;
