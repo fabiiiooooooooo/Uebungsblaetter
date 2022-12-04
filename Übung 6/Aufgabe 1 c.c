c32c0b4f24c1aafaee3c819bc032565ac12479a4
breadsident <116891845+breadsident@users.noreply.github.com>
2022-12-04 19:08:18 +0100

    Update Aufgabe 1 c.c

--- Übung 6/Aufgabe 1 c.c
+++ Übung 6/Aufgabe 1 c.c
@@ -1,25 +1,5 @@
 #include <stdio.h>
 
-
-
-/*void switchfunktion (double n)
-{
-        n = 0;
-
-        switch(n) {
-                case 0 :
-                        printf ("false\n");
-                        break;
-                case  1 || -1 :
-                        printf("unclear\n");
-                        break;
-                default:
-                        printf("true\n");
-                        break;
-        }
-}
-*/
-
 int main (void)
 {
         int n;
