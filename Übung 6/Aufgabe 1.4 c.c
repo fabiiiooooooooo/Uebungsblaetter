fe5fb77593c63bb4133195aaed2edc151cd88b7a
breadsident <116891845+breadsident@users.noreply.github.com>
2022-12-04 19:07:28 +0100

    Update Aufgabe 1.4 c.c

--- Übung 6/Aufgabe 1.4 c.c
+++ Übung 6/Aufgabe 1.4 c.c
@@ -1,35 +1,14 @@
-#include <stdio.h>
 
-void f(void)
-{
-        int x;
-        int y;
 
-        x = 5;
-        y = x + 3;
-}
-
-void  dfdx(int x, int y)
-{
-        x = x+2;
-}
-
-void function(int x)
+int function(void)
 {
         int i = 1;
         int n = 0;
 
-        while (1) {
-                x -= f(x) / dfdx(x);
-                if (i == n) {
-                                break;
-                }
-                ++i;
-        }
-
         for(i = 1; i != n; ++i){
                 x -= f(x) / dfdx(x);
         }
+    return 0;
 }
 
 int main(void)
