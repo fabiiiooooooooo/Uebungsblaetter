1c62cf4ad56ae2d62eb7b15ff98532d8d4a6a4f1
DasWildeMaus <116891782+DasWildeMaus@users.noreply.github.com>
2022-12-19 00:15:20 +0100

    Version 2.0

--- Blatt 8/32.c
+++ Blatt 8/32.c
@@ -1,31 +1,62 @@
 #include "ZAHLENFOLGEN.h"
 
-#define LEN 5
-
 int main(){
         int i;
-        unsigned int array_a[LEN];
-        unsigned int array_b[LEN];
-        unsigned int array_c[LEN];
-        double big_array_a[LEN];
-        double big_array_b[LEN];
-        double big_array_c[LEN];
-        double ratio[LEN];
+        int len = 5;
+
+        unsigned int array_a[len];
+        unsigned int array_b[len];
+        unsigned int array_c[len];
+        double big_array_a[len];
+        double big_array_b[len];
+        double big_array_c[len];
 
         unsigned int b1 = 2308U;
         unsigned int b2 = 4261U;
 
-        for (i = 0; i <= LEN; i++){
+        for (i = 0; i <= len; i++){
                 array_a[i] = fibonacci();
                 array_b[i] = brady_seq();
         }
-        init_brady(b1, b2);
-        for (i = 0; i <= LEN; i++){
+
+        printf("\n");
+        for (i = 0; i <= len; i++){
+                printf("%d", array_a[i]);                       //Test-Ausgabe Fibo
+        }
+        printf("\n");
+        for (i = 0; i <= len; i++){
+                printf("%d", array_b[i]);                       //Test-Ausgabe Brady
+        }
+
+        void init_brady();                                      //Usereingabe Brady Array C
+        for (i = 0; i <= len; i++){
                 array_c[i] = brady_seq();
         }
 
-        calc_sequence_ratio(array_a, ratio, LEN);
-        calc_sequence_ratio(array_b, ratio, LEN);
-        calc_sequence_ratio(array_c, ratio, LEN);
+        calc_sequence_ratio(array_a, big_array_a, len);         //Verhältnisse
+        calc_sequence_ratio(array_b, big_array_b, len);
+        calc_sequence_ratio(array_c, big_array_c, len);
+
+        printf("\n");
+        for (i = 0; i <= len; i++){                             //Test-Ausgabe User-Brady
+                printf("%d", array_c[i]);
+        }
+        printf("\n");
+        for (i = 0; i <= len; i++){
+                printf("%d", big_array_a[i]);                   //Test-Ausgabe Verhältnis 1
+        }
+        printf("\n");
+        for (i = 0; i <= len; i++){
+                printf("%d", big_array_b[i]);                   //Test-Ausgabe Verhältnis 2
+        }
+        printf("\n");
+        for (i = 0; i <= len; i++){
+                printf("%d", big_array_c[i]);                   //Test-Ausgabe Verhältnis 3
+        }
+        printf("\n");
+
+
+
+
         return 0;
 }
--- Blatt 8/Zahlenfolgen.c
+++ Blatt 8/Zahlenfolgen.c
@@ -3,13 +3,13 @@
 #define SCANF_USER(a) (scanf("%d", &(a)))
 
 unsigned int fibonacci(void){
+        int i;
         static int n = 5;
         static unsigned int fib = 1;
-        int i;
         int f1 = 0;
         int f2 = 1;
 
-        for(int i = 1; i <= n; i++){
+        for(i = 1; i <= n; i++){
                 fib = f1 + f2;
                 f2 = fib;
                 f1 = f2;
@@ -18,12 +18,12 @@ unsigned int fibonacci(void){
 }
 
 unsigned int brady_seq(void){
-        static int n = 5;
-        static unsigned int bsq = 1; //bsq -> BradySeQuence
         int i;
         unsigned int b1, b2;
+        static int n = 5;
+        static unsigned int bsq = 1; //bsq -> BradySeQuence
 
-        for(int i = 1; i <= n; i++){
+        for(i = 1; i <= n; i++){
                 bsq = b1 + b2;
                 b2 = bsq;
                 b1 = b2;
@@ -42,7 +42,7 @@ void init_brady(unsigned int b1, unsigned int b2){
 
 void calc_sequence_ratio(unsigned int seq[], double ratio[], int len){
         int i;
-        for(int i = 0; i < len; i++){
+        for(i = 0; i < len; i++){
                 ratio[i] = (double) seq[i+1] / seq[i];
         }
 }
